7ca5b3df5ed59bb91b4c4cf8c707a68703326cef
bmg004 <bridget.geary@nationwidechildrens.org>
2016-02-14 17:09:22 -0500

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,20 +1,12 @@
-household_power_consumption <- read.csv("C:/Users/bmg004/Desktop/Coursera/household_power_consumption.txt", header=FALSE, sep=";")
-power <- read.table(file, header=T, sep=";")
-power$Date <- as.Date(power$Date, format="%d/%m/%Y")
-df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
-df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
-df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
-df$Voltage <- as.numeric(as.character(df$Voltage))
-df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
-df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
-df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
-df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
-plot1 <- function() {
-  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
-  dev.copy(png, file="plot1.png", width=480, height=480)
-  dev.off()
-  cat("Plot1.png has been saved in", getwd())
-}
-plot1()
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
