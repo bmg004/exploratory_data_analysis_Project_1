5aa84de925832e72d96be83e05abd1cf19f3a4e0
bmg004 <bridget.geary@nationwidechildrens.org>
2016-02-14 15:39:41 -0500

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,5 +1,5 @@
 ##Plot_1_File
-
+@@ -0,0 +1,9 @@
 dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
